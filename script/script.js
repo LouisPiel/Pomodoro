2c8fdc6135d966c8f64c9062a5ebd0eccf35ade5
Louis <louis.piel@etu.unicaen.fr>
2023-09-13 22:29:23 +0200

    implementation de la fonction renitialiser et modif pour les boutons

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let Travail = document.getElementById("travail");
+let Pause = document.getElementById("pause");
+
+let TravailMinutes = 30;
+let PauseMinutes = 30;
+let secondes = 0;
+
+
+
+function renitialiser() {
+    location.reload();
+}
