29197ad46d41356a20aac73838df6333a4eb29f2
Louis <louis.piel@etu.unicaen.fr>
2023-09-16 01:06:08 +0200

    Suite fonctionnement temps (fonctionne partiellement)

--- script/script.js
+++ script/script.js
@@ -1,47 +1,46 @@
 let Travail = document.getElementById("travail");
 let Pause = document.getElementById("pause");
 
-let TravailTemps = 30;
+var TravailTemps = 0;
 let PauseTemps = 30;
-let secondes = 0;
+let secondes = 10;
 
-function affichageTemps(crono) {
-    let TempsSec = TravailTemps + ":" + (secondes < 10 ? "0" : "") + secondes;
-    return TempsSec;
-}
+function affichageTemps (chrono) {
+    let chronoString = chrono.toString()
+    chronoString = chronoString.length < 2 ? '0' + chronoString : chronoString
+    return chronoString
+  }
 
 
 
 window.onload = () => {
     document.getElementById('minutes').innerHTML = affichageTemps(TravailTemps);
     document.getElementById('secondes').innerHTML = affichageTemps(secondes);
-    TravailTemps.classList.add('active')
 }
 
 function démarrer() {
     document.getElementById('démarrer').style.display = 'none';
     document.getElementById('rénitialiser').style.display = 'block';
 
-    secondes = 59;
+    secondes = 10;
 
-    let TravailMinute = TravailTemps - 1;
-    let PauseMinute = PauseTemps - 1;
+    let TravailMinute = TravailTemps;
+    let PauseMinute = PauseTemps ;
 
-    let changement = false;
+     changement = false;
 
     let temps = () => {
         document.getElementById('minutes').innerHTML = affichageTemps(TravailMinute);
         document.getElementById('secondes').innerHTML = affichageTemps(secondes);
 
         secondes = secondes - 1;
-
         if (secondes === 0) {
             TravailMinute = TravailMinute - 1;
             if (TravailMinute === -1) {
                 if (!changement) {
                     TravailMinute = PauseMinute;
                     changement = true;
-
+                    
                     TravailTemps.classList.remove('active');
                     PauseTemps.classList.add('active');
                 } else {
