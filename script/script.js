d8b4f57a7f5a6b3cc07026fe569765066f487727
Louis <louis.piel@etu.unicaen.fr>
2023-09-16 18:40:28 +0200

    defilement du temps fonctionnel

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 let Travail = document.getElementById("travail");
 let Pause = document.getElementById("pause");
 
-var TravailTemps = 0;
-let PauseTemps = 30;
-let secondes = 10;
+var TravailTemps = 1;
+let PauseTemps = 1;
+let secondes = 0;
 
 function affichageTemps (chrono) {
     let chronoString = chrono.toString()
@@ -22,10 +22,10 @@ function démarrer() {
     document.getElementById('démarrer').style.display = 'none';
     document.getElementById('rénitialiser').style.display = 'block';
 
-    secondes = 10;
+    secondes = 59;
 
-    let TravailMinute = TravailTemps;
-    let PauseMinute = PauseTemps ;
+    let TravailMinute = TravailTemps - 1;
+    let PauseMinute = PauseTemps - 1;
 
      changement = false;
 
@@ -34,21 +34,24 @@ function démarrer() {
         document.getElementById('secondes').innerHTML = affichageTemps(secondes);
 
         secondes = secondes - 1;
-        if (secondes === 0) {
+        if (secondes == 0) {
             TravailMinute = TravailMinute - 1;
-            if (TravailMinute === -1) {
+            if (TravailMinute == -1) {
                 if (!changement) {
                     TravailMinute = PauseMinute;
+                    secondes = 60;
                     changement = true;
                     
                     TravailTemps.classList.remove('active');
                     PauseTemps.classList.add('active');
                 } else {
-                    TravailMinute = PauseMinute - 1;
+                    TravailMinute = TravailTemps;
+                    secondes = 1;
                     changement = false;
 
                     PauseTemps.classList.remove('active');
                     TravailTemps.classList.add('active');
+                    
 
                 }
             }
